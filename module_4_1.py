e9c8e0ef8f177a49fa2c0d5cacbe5953f421e7e2
Dzhamil <Dzheeem@yandex.ru>
2024-09-25 20:19:58 +0700

    task completed

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as math
+from true_math import divide as high_math
+
+result1 = math(69, 3)
+result2 = math(3, 0)
+result3 = high_math(49, 7)
+result4 = high_math(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
